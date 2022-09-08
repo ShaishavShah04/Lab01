64c38d0ed5af6991a0e50e572950a6e7417085f5
Shaishav <shaishav.shah04@gmail.com>
2022-09-08 15:54:31 -0600

    Update URL

--- version_check.py
+++ version_check.py
@@ -2,6 +2,6 @@ import requests as r
 
 # print(r.__version__)
 
-res = r.get("https://raw.githubusercontent.com/ShaishavShah04/Crypto-Scanner/main/scanner.py")
+res = r.get("https://raw.githubusercontent.com/ShaishavShah04/Lab01/master/version_check.py")
 
 print(res.text)
